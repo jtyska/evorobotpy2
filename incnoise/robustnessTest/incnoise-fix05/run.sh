72c3b9bfa48523cc8e62665b9f1c7bc7605607d4
jtyska <jonata.tyska@ufsc.br>
2021-12-30 14:17:53 +0100

    sending the fixed noise results

---
+++ incnoise/robustnessTest/incnoise-fix05/run.sh
@@ -0,0 +1,20 @@
+cd ./xhopper
+rm *.fit
+rm *.out
+./run.sh
+cd ../xhalfcheetah
+rm *.fit
+rm *.out
+./run.sh
+cd ../xant
+rm *.fit
+rm *.out
+./run.sh
+cd ../xwalker
+rm *.fit
+rm *.out
+./run.sh
+cd ../xhumanoid
+rm *.fit
+rm *.out
+./run.sh
