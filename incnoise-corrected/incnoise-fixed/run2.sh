b81d92c6a6ac7cea8d7d03ed4b92e2ba20b7ed7b
Jonata <jonata.tyska@ufsc.br>
2022-03-02 18:07:34 -0300

    sending corrected version of incnoise experiments

---
+++ incnoise-corrected/incnoise-fixed/run2.sh
@@ -0,0 +1,20 @@
+cd ./xhopperv0
+rm *.fit
+rm *.out
+./run.sh
+cd ../xhalfcheetahv0
+rm *.fit
+rm *.out
+./run.sh
+cd ../xantv0
+rm *.fit
+rm *.out
+./run.sh
+cd ../xwalkerv0
+rm *.fit
+rm *.out
+./run.sh
+cd ../xhumanoidv0
+rm *.fit
+rm *.out
+./run.sh
